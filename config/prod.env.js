8a8d6e3ecbff75925fb20828ae13481ecd88fcd7
luyanan <1271778060@qq.com>
2019-06-16 09:25:01 +0800

    config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '""',
+  BASE_API: '"http://152.136.52.66:8090"',
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,7 @@ import store from '../store'
 import { getToken } from '@/utils/auth'
 // 创建axios实例
 const service = axios.create({
-  baseURL: process.env.BASE_API + '/api', // api的base_url
+  baseURL: process.env.BASE_API, // api的base_url
   timeout: 15000 // 请求超时时间
 })
 
